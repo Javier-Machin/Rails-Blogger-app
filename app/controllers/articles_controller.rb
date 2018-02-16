67d5378debf78d2f053192475c473595558ebeab
Javier Machin <javier_machin_acosta@hotmail.com>
2018-02-16 16:19:40 +0000

    Change the root to be the articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,15 @@ class ArticlesController < ApplicationController
   	@article = Article.new(article_params)
   	@article.save
 
+  	flash.notice = "Article '#{@article.title}' Created!"
+
   	redirect_to article_path(@article)
   end
 
   def destroy
   	@article = Article.destroy(params[:id])
+
+  	flash.notice = "Article '#{@article.title}' Destroyed!"
 
   	redirect_to articles_path
   end
@@ -33,7 +37,9 @@ class ArticlesController < ApplicationController
   	@article = Article.find(params[:id])
   	@article.update(article_params)
 
+  	flash.notice = "Article '#{@article.title}' Updated!"
+
   	redirect_to article_path(@article)
   end
-  
+
 end
