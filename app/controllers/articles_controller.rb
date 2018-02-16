7897306399c89d164d7f536f85145a125557e990
Javier Machin <javier_machin_acosta@hotmail.com>
2018-02-16 16:08:37 +0000

    Add article editing functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,16 @@ class ArticlesController < ApplicationController
 
   	redirect_to articles_path
   end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	@article.update(article_params)
+
+  	redirect_to article_path(@article)
+  end
+  
 end
