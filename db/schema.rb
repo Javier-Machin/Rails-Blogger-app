6c8aa8f0a1dd1d9cb4ff8499ef626aa6cb29e0b8
Javier Machin <javier_machin_acosta@hotmail.com>
2018-02-17 16:01:44 +0000

    Add paperclip gem, add articles images feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217095611) do
+ActiveRecord::Schema.define(version: 20180217153707) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_uploaded_at"
   end
 
   create_table "comments", force: :cascade do |t|
